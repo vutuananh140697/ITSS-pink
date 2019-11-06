05d69c6a66ff98978a086793b466d73d338d9027
EC2 Default User <ec2-user@ip-172-31-82-208.ec2.internal>
2019-11-06 15:15:54 +0000

    search_place

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
   end
+  
+  def search
+    if params[:name].blank? 
+      # && params[:start_date].blank? && params[:end_date].blank?
+      redirect_to root_path
+    else
+      @places = Place.get_places(params[:name])
+    end
+  end
 end
